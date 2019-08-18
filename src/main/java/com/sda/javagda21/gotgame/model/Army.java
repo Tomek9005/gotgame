73360b61e27cac3d1d006a22e11be494ba9e2461
Prezez <47228750+Prezez@users.noreply.github.com>
2019-08-18 11:56:24 +0200

    Army class updated - fight methods added

--- src/main/java/com/sda/javagda21/gotgame/model/Army.java
+++ src/main/java/com/sda/javagda21/gotgame/model/Army.java
@@ -1,6 +1,31 @@
 package com.sda.javagda21.gotgame.model;
 
 
+import java.util.Random;
+
 public class Army {
     private int size;
+
+    public Army(int size) {
+        this.size = size;
+    }
+
+    public int dealDmg (int size) {
+        Random random = new Random();
+        int damage = 0;
+
+        for (int i = 0; i < size; i++) {
+            damage = damage + random.nextInt(3);
+        }
+
+        return damage;
+
+    }
+
+    public int takeDmg (int dmg, int size){
+        int kills = dmg/3;
+        return size - kills;
+    }
 }
+
+
