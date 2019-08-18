a5623f35a142b941e49e3ea73f9556ae15527533
Prezez <47228750+Prezez@users.noreply.github.com>
2019-08-18 11:12:04 +0200

    Initial commit

---
+++ src/main/java/com/sda/javagda21/gotgame/GotgameApplication.java
@@ -0,0 +1,13 @@
+package com.sda.javagda21.gotgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotgameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GotgameApplication.class, args);
+    }
+
+}
